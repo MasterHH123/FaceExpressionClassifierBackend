b38c8c2d9320067993de90f04fb8c61f48058916
Horacio H <hernandezh4@hotmail.com>
2025-04-17 13:03:42 -0700

    Added CORS to allow traffic from my local frontend react app.

--- server.go
+++ server.go
@@ -16,6 +16,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/gin-contrib/cors"
 )
 
 const (
@@ -81,7 +82,7 @@ func login(c *gin.Context){
 	}
 
 	c.JSON(http.StatusOK, gin.H{"token": token})
-	
+
 }
 
 func authenticateMiddleware(c *gin.Context){
@@ -233,6 +234,19 @@ func predictHandler(c *gin.Context) {
 
 func main(){
 	router := gin.Default()	
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:	[]string{
+			"http://localhost:5173",
+			"http://localhost:5173/login",
+			"http://localhost:5173/predict",
+		},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Authorization", "Content-Type"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge: 12 * time.Hour,
+	}))	
 
 	router.POST("/login", login)
 	router.POST("/predict", authenticateMiddleware, predictHandler)
